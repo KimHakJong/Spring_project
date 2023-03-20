ede7c871e41fd85a4fba60bbb5364e81515498cd
김학종 <@DESKTOP-97QKNCN.mshome.net>
2023-03-20 15:36:50 +0900

    commit

--- Gant/src/main/java/com/gant/myhome/hakjong/domain/MySaveFolder.java
+++ Gant/src/main/java/com/gant/myhome/hakjong/domain/MySaveFolder.java
@@ -1,16 +1,15 @@
-package com.gant.myhome.hakjong.domain;
+/*package com.gant.myhome.hakjong.domain;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "my")
-/*
+
   application.properties에서 설정한 내용 중
   my로 시작하는 키에 대한 값을 필드에 주입합니다.
   my.savefolder=c:/upload
   my.sendfile=c:\Users\pwj58\image\koala.jpg
- */
 
 public class MySaveFolder {
   private String savefolder;
@@ -34,3 +33,4 @@ public class MySaveFolder {
 	}
   
 }
+*/
