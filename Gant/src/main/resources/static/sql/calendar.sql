de7ea95f9d96c2fb4882ced4af1b6e2e45760850
kylep <kylep@DESKTOP-TA137DH>
2023-04-14 09:16:32 +0900

    0414

--- Gant/src/main/resources/static/sql/calendar.sql
+++ Gant/src/main/resources/static/sql/calendar.sql
@@ -3,23 +3,23 @@ drop table members cascade constraints purge;
 drop table project cascade constraints purge;
 
 create table calendar(
-id  varchar2(15), 
+id  varchar2(100), 
 allday varchar2(15) default 'true',
 startday varchar2(30),
 endday varchar2(30),
 name varchar2(30) references members(id) on delete cascade,
-title varchar2(200)
+title varchar2(2000)
 );
 
 create table pcalendar(
-id  varchar2(15), 
-p_no number, 
+id  varchar2(100), 
+p_no number references project(p_no) on delete cascade, 
 hostid varchar2(15),  --host도 admin 권한을 가진다
 allday varchar2(15) default 'true',
 startday varchar2(30),
 endday varchar2(30),
 name varchar2(30) references members(id) on delete cascade,
-title varchar2(200)
+title varchar2(2000)
 );
 
 insert into calendar values('admin', 'true', 'true', '2023-03-20', '2023-03-21', '프로젝트', '프로젝트 기간');  
